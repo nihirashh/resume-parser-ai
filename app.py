c5ba7bde19892d3583aac127a050c511851d4657
Nihira Shrivastava <nihirashrivastava@gmail.com>
2026-04-19 20:37:58 +0530

    Implemented file upload functionality

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Resume Parser is Running 🚀"
+    if request.method == "POST":
+        file = request.files["resume"]
+        
+        if file:
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+            file.save(filepath)
+            return "File uploaded successfully ✅"
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
